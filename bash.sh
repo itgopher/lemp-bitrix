b8a1e9105b0a2e56b23c9c705d230f45955bc474
Dmitriy <bigtroble@gmail.com>
2020-05-07 20:09:19 +0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -5,13 +5,6 @@ sudo apt install htop
 sudo apt install mc
 
 # Install Nginx
-sudo bash -c 'cat > /etc/apt/sources.list.d/nginx.list << EOL
-deb http://nginx.org/packages/ubuntu/ xenial nginx
-deb-src http://nginx.org/packages/ubuntu/ xenial nginx
-EOL'
-
-wget http://nginx.org/keys/nginx_signing.key
-sudo apt-key add nginx_signing.key
 sudo apt-get update
 sudo apt-get install nginx
 
