82df2837e259609348019ceb0eb44909b741e171
Dmitriy <bigtroble@gmail.com>
2020-05-08 19:06:07 +0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -16,10 +16,9 @@ sudo service mysql start
 sudo mysql_secure_installation
 
 # Install PHP 7.4
-sudo apt-get install php
-
-# Install needed modules for PHP
-sudo apt-get install php7.4-fpm php7.4-mysql php7.4-curl php7.4-gd php7.4-bz2 php7.4-mbstring php7.4-xml php7.4-zip
+sudo add-apt-repository ppa:ondrej/php
+sudo apt update
+sudo apt install php7.4-fpm php7.4-common php7.4-mysql php7.4-xml php7.4-xmlrpc php7.4-curl php7.4-gd php7.4-imagick php7.4-cli php7.4-dev php7.4-imap php7.4-mbstring php7.4-opcache php7.4-soap php7.4-zip unzip -y
 
 # Install Composer (PHP dependencies manager)
 ## First install php-cli, unzip, git, curl, php-mbstring
